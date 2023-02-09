02b2b0f2fac46b44709fa1da34b0203bb4117c70
osteen <osteen.mutinda@student.moringaschool.com>
2023-02-09 03:50:02 +0300

    Completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,16 @@
 class Dog
  # attr_accessor :this_dogs_name -makes it easier to  access and modify the value  after "attr_accessor: " i am writing this for future refernce since its not needed in this case ,,,revision yaani
 
-  def name=(new_name)
-    @this_dogs_name = new_name
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
 
   def name
     @this_dogs_name
   end   
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
